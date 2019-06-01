8f93eb93a5f5fdc9b8cb2e30eb9b419d4d604837
Sahar <saharco@campus.technion.ac.il>
2019-06-01 16:04:01 +0300

    added fixed avl tree to master branch

--- base/library/src/test/kotlin/AVLTreeTest.kt
+++ base/library/src/test/kotlin/AVLTreeTest.kt
@@ -1,61 +1,67 @@
-import il.ac.technion.cs.softwaredesign.database.AVLTree
-import il.ac.technion.cs.softwaredesign.database.CourseAppDatabaseFactory
 import il.ac.technion.cs.softwaredesign.mocks.SecureStorageMock
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-var charset = Charsets.UTF_8
-class AVLTreeTest {
-    @Test
-    internal fun `basic test` () {
-        val tree = AVLTree(SecureStorageMock())
-        tree.insert("yuval".toByteArray(charset), "nahon".toByteArray(charset))
-        println(tree)
-        println(tree.root?.size())
-        tree.insert("sahar".toByteArray(charset), "cohen".toByteArray(charset))
-        println(tree)
-        println(tree.root?.size())
-        tree.insert("yuval1".toByteArray(charset), "nahon".toByteArray(charset))
-        println(tree)
-        println(tree.root?.size())
-        tree.insert("sahar1".toByteArray(charset), "cohen".toByteArray(charset))
-        println(tree)
-        println(tree.root?.size())
-        tree.insert("sahar2".toByteArray(charset), "cohen".toByteArray(charset))
-        println(tree)
-        println(tree.root?.size())
-    }
-    @Test
-    internal fun `adding a lot of elements` () {
-        val tree = AVLTree(SecureStorageMock())
-        for (i in 0..100) {
-            tree.insert(("yuval" + i).toByteArray(charset), ("nahon" + i).toByteArray(charset))
-        }
-        for (i in 0..100) {
-            assertEquals(tree.search("yuval$i".toByteArray(charset))?.toString(charset), "nahon$i")
-        }
-        assertEquals(tree.size(), 101)
-        println(tree.root?.height)
-        assertEquals(tree.search("yuval2".toByteArray(charset))?.toString(charset), "nahon2")
-    }
-    @Test
-    internal fun `delete TONS of elements` () {
-        val tree = AVLTree(SecureStorageMock())
-        for (i in 0..9) {
-            tree.insert(("yuval" + i).toByteArray(charset), ("nahon" + i).toByteArray(charset))
-            assertEquals(i + 1, tree.size())
-        }
 
-        //tree.delete("yuval3".toByteArray(charset))
-        //tree.delete("yuval4".toByteArray(charset))
-        //tree.delete("yuval6".toByteArray(charset))
-//        tree.delete("yuval7".toByteArray(charset))
-//        tree.delete("yuval8".toByteArray(charset))
-//        tree.delete("yuval9".toByteArray(charset))
-//        tree.delete("yuval10".toByteArray(charset))
-//        println(tree.size())
-//        tree.printTree()
-//        assertEquals(tree.size(), 11)
-//        println(tree.root?.height)
-//        assertEquals(tree.search("yuval2".toByteArray(charset))?.toString(charset), "nahon2")
+var charset = Charsets.UTF_8
+
+class AVLTreeTest {
+
+    @Test
+    internal fun `inserting one element and reading it`() {
+        val storage = SecureStorageMock()
+        val tree = AVLTree(storage)
+        tree.insert("1000000-2019".toByteArray(charset), "nahon".toByteArray(charset))
+        assertTrue("nahon".toByteArray(charset).contentEquals(tree.search("1000000-2019".toByteArray(charset))!!))
     }
+
+    @Test
+    internal fun `inserting multiple elements and reading them`() {
+        val storage = SecureStorageMock()
+        val tree = AVLTree(storage)
+        for (i in 0..100) {
+            tree.insert(("100000$i-2019").toByteArray(charset), "nahon$i".toByteArray(charset))
+        }
+        for (i in 0..100) {
+            assertTrue("nahon$i".toByteArray(charset).contentEquals(tree.search(("100000$i-2019").toByteArray(charset))!!))
+        }
+    }
+
+    @Test
+    internal fun `inserting one element and deleting it`() {
+        val storage = SecureStorageMock()
+        val tree = AVLTree(storage)
+        tree.insert("1000000-2019".toByteArray(charset), "nahon".toByteArray(charset))
+        tree.delete("1000000-2019".toByteArray(charset))
+    }
+
+    @Test
+    internal fun `inserting multiple elements and deleting them`() {
+        val storage = SecureStorageMock()
+        val tree = AVLTree(storage)
+        val list: MutableList<Int> = (0..100).toMutableList()
+        list.shuffle()
+        for (i in list) {
+            tree.insert(("100000$i-2019").toByteArray(charset), "nahon$i".toByteArray(charset))
+        }
+        for (i in list.reversed()) {
+            assertTrue("nahon$i".toByteArray(charset).contentEquals(tree.search("100000$i-2019".toByteArray(charset))!!))
+            tree.delete("100000$i-2019".toByteArray(charset))
+            assertEquals(null, tree.search("100000$i-2019".toByteArray(charset)))
+        }
+    }
+
+    @Test
+    internal fun `inserting and deleting one by one`() {
+        val storage = SecureStorageMock()
+        val tree = AVLTree(storage)
+        val list: MutableList<Int> = (0..100).toMutableList()
+        list.shuffle()
+        for (i in list) {
+            tree.insert(("100000$i-2019").toByteArray(charset), "nahon$i".toByteArray(charset))
+            assertTrue("nahon$i".toByteArray(charset).contentEquals(tree.search("100000$i-2019".toByteArray(charset))!!))
+            tree.delete("100000$i-2019".toByteArray(charset))
+            assertEquals(null, tree.search("100000$i-2019".toByteArray(charset)))
+        }
+    }
+
 }
